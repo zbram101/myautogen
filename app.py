8f70bce0f94feae89ef296631717a0458b89e86e
Bharadwaj Ramachandran <bharadwajramachandran@Bharadwajs-MacBook-Pro.local>
2023-10-28 05:45:32 -0700

    group agents working together with user correctly

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ alert_idetntifer = AssistantAgent("alert_idetntifer", llm_config={"config_list":
 stuck_idetntifer = AssistantAgent("stuck_idetntifer", llm_config={"config_list": config_list}, system_message="Your goal is to identify if the user is stuck or is not willing to share things with you. If you identify they are stuck, reply STUCK. Otherwise, reply CONTINUE.")
 
 user_proxy = UserProxyAgent("user_proxy", 
-                            human_input_mode="TERMINATE", 
+                            human_input_mode="ALWAYS", 
                             max_consecutive_auto_reply=10,
                             code_execution_config={"work_dir": "coding"},
-                            llm_config={"config_list": config_list},
-                            system_message="Your goal is to formulate a response. There are some things you need to consider when responding to the user. 1) If the user has mentioned any dangerour or illegal activities in the chat, you should reply with OH NO BAD IDEA. 2) if the user seems stuck in the conversation reply with OH NO YOUR STUCK"
+                            llm_config=False,
+                            system_message="Formulate a response to user in the conversation. There are some things you need to consider when responding to the user. 1) if user has mentioned any dangerour or illegal activities in the chat, you should TERMINATE with OH NO BAD IDEA. 2) if the user seems stuck in the conversation you should TERMINATE OH NO YOUR STUCK"
                                                    )
 
 
@@ -23,5 +23,5 @@ groupchat = GroupChat(agents=[user_proxy, alert_idetntifer, stuck_idetntifer], m
 manager = GroupChatManager(groupchat=groupchat, llm_config={"config_list": config_list})
 
 
-user_proxy.initiate_chat(manager, message="AI: how are you User: I want to kill myself")
+user_proxy.initiate_chat(manager, message="AI: how are you User: I dont know")
 # This initiates an automated chat between the two agents to solve the task
